fff8e0879b151dd3a610b85133ef0bcd42bbde1e
Charl Christopher M. Peñada <cmpenada@student.apc.edu.ph>
2026-02-17 16:22:30 +0000

    Updated the API Url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/guestbook'; 
+const API_URL = 'https://simple-react-nest-js-supabase-app.vercel.app/'; 
 
 function App() {
   const [messages, setMessages] = useState([]);
